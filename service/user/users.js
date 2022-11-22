1b4d2e45ccd8a2e7a3e0ec69b7d826a1847b43a6
Geon gyu Park <geongyupark@Geonui-MacBookAir-2.local>
2022-11-22 18:24:20 +0900

    feat: Add hash password feature

--- service/user/users.js
+++ service/user/users.js
@@ -30,7 +30,6 @@ router.post('/login', ( req, res ) => {
     });
 });
 
-
 router.post('/sign-up', ( req, res ) => {
     let regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
     let regPw = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
