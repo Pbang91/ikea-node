10656880bd1c670c273ee646289a8170a29e642b
Geon gyu Park <geongyupark@Geonui-MacBookAir-2.local>
2022-11-22 18:23:45 +0900

    feat: Add hashed password feature

--- service/user/users.js
+++ service/user/users.js
@@ -1,9 +1,37 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 const getConnection = require('./../../config/database');
 
+router.post('/login', ( req, res ) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    getConnection(( conn ) => {
+        let sql = `SELECT * FROM users WHERE email="${email}"`
+
+        conn.query(sql, ( err, row ) => {
+            if (err) {
+                console.log(err);
+                throw err;
+            } else if ( row.length == 0 ) {
+                res.status(400).send({message : "Invalid Password"});
+            } else {
+                let user = row[0];
+                // console.log(row[0].password);
+                let checkPassword = bcrypt.compareSync(password, user.password);
+
+                if (checkPassword) {
+                    res.status(200).send({message : "Success", userId : user.id});
+                } else {
+                    res.status(400).send({message : "Invalid Password"});
+                }
+            }
+        });
+    });
+});
+
+
 router.post('/sign-up', ( req, res ) => {
-    let sql = `INSERT INTO users (email, password) VALUES(?, ?)`;
-    let param = [req.body.email, req.body.password];
     let regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
     let regPw = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
     
@@ -11,12 +39,15 @@ router.post('/sign-up', ( req, res ) => {
         res.send('Invalid User Information');
     } else {
         getConnection(( conn ) => {
+            let sql = `INSERT INTO users (email, password) VALUES(?, ?)`;
+            let hashedPassword = bcrypt.hashSync(req.body.password, 10);
+            let param = [req.body.email, hashedPassword];
             conn.query(`SELECT * FROM users WHERE email="${req.body.email}"`, ( err, row ) => {
                 if (err) {
                     console.log(err);
                     throw err;
                 } else if ( row.length > 0 ) {
-                    res.send('Already Exists eamil');
+                    res.status(400).send('Already Exists eamil');
                 } else {
                     conn.query(sql, param, ( err, result, field) => {
                         if (err) {
